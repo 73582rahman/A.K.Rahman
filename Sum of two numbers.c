5bbfe54c96c52d77b1f30dce9361693c0ccb3a90
73582rahman <109493782+73582rahman@users.noreply.github.com>
2022-09-19 08:10:50 +0530

    Create Sum of two numbers.c

---
+++ Sum of two numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a,b,c;
+Printf("enter the a value:");
+scanf("%d",&a);
+printf("enter the b value:");
+scanf("%d",&b);
+c=a+b;
+Printf(" The sum of two numbers:",c);
+}
