95be806de0668f3a7a742a0411245306e4f4b626
Khaleelur Rahman A <109493782+73582rahman@users.noreply.github.com>
2025-02-23 06:11:42 -0800

    Add files via upload

---
+++ practice4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class practice4
+{
+    public static void main(String args[])
+    {
+        Scanner prac4=new Scanner(System.in);
+
+        String rcb=prac4.nextLine();
+        if(rcb.equals("win"))
+        {
+            System.out.println("Ee sala cup namde");
+        }
+        else{
+            System.out.println("Ee sala cup lollipopu");
+        }
+    }
+}
