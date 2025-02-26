e9bd0cc098867985c3942d81c96efc29a2104445
Khaleelur Rahman A <109493782+73582rahman@users.noreply.github.com>
2025-02-26 09:33:40 -0800

    Add files via upload

---
+++ count.java
@@ -0,0 +1,21 @@
+
+  class count
+  {
+    public static void main(String args[])
+    {
+       int oddcount=0;
+       for(int i=0;i<=10;i++)
+       {
+        if(i%2==0)
+        {
+        }
+        else
+        {
+             oddcount=oddcount+1;
+            System.out.println(oddcount);
+           // System.out.println("odd number:"+i);
+        }
+       }
+       System.out.println(oddcount);
+    }
+  }
